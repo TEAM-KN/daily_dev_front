1b53b8407bce9219de7f9d70602d88fb9fc041c4
scnoh <stc9606@naver.com>
2023-05-20 15:49:50 +0900

    vite config 추가

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const proxyOptions = {
+    target: 'dev server host...',
+    changeOrigin: true
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/dailyf',
+    server: {
+        proxy: {
+            '/dailyb': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true
+            }
+        }
+    },
+    plugins: [react()],
+})
