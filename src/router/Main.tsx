1ed874fdf561df8b17c5d18ceb2dd2a7637fbb7a
kkomyoung <kya754@gmail.com>
2023-06-02 18:33:30 +0900

    feat: 회원가입 화면 추가

--- src/router/Main.tsx
+++ src/router/Main.tsx
@@ -2,14 +2,18 @@ import {RouteObject} from 'react-router-dom'
 import {lazy} from 'react'
 
 const Main = lazy(() => import('../view/Main'))
+const Register = lazy(() => import('../view/Register'))
 
 const MainRoutes: RouteObject = {
     path: '/',
-    element: <Main />,
     children: [
         {
             path: '',
             element: <Main />
+        },
+        {
+            path: '/register',
+            element: <Register />
         }
     ]
 }
