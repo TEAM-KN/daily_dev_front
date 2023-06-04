e236f7c57a24bd90b9f79062204dca7839f984c8
kkomyoung <kya754@gmail.com>
2023-06-05 00:22:11 +0900

    fix: 로고 대체텍스트 수정

--- src/view/Register.tsx
+++ src/view/Register.tsx
@@ -51,11 +51,11 @@ export default function Register() {
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Daily Dev</span>
               <img
                 className="h-8 w-auto"
                 src={Logo}
-                alt=""
+                alt="Daily Dev"
               />
             </a>
           </div>
