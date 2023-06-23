ae2a995c9d921c2b355c38aeb43c1b5a9cf5873a
kkomyoung <kya754@gmail.com>
2023-06-23 14:58:43 +0900

    #10 Main 라우터 경로 수정

--- src/router/Main.tsx
+++ src/router/Main.tsx
@@ -4,18 +4,16 @@ import {lazy} from 'react'
 const Landing = lazy(() => import('../view/Landing'))
 const Register = lazy(() => import('../view/Register'))
 
-const MainRoutes: RouteObject = {
-    path: '/',
-    children: [
-        {
-            path: '',
-            element: <Landing />
-        },
-        {
-            path: '/register',
-            element: <Register />
-        }
-    ]
-}
+const MainRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <Landing/>
+    },
+    {
+        path: '/register',
+        element: <Register />
+    }
+]
+    
 
 export default MainRoutes
