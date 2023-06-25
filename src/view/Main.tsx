51288b85942327a23ae2e3c7bcb6fb44ab16d065
kkomyoung <kya754@gmail.com>
2023-06-25 21:09:42 +0900

    #12 더미 데이터 테스트

--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -4,6 +4,42 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 import * as Apis from '../service/apis'
 
 export default function Main() {
+  const data = [
+    {
+      contentId: 162,
+      siteCode: null,
+      title: '가장 강력한 ‘맥’ 제품…애플, 맥 스튜디오·맥 프로 공개',
+      description:
+        '사진=애플 애플은 5일(현지시간) 미국 캘리포니아주 쿠퍼티노 애플파크에서 열린 ‘세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 세계 개발자 회의’(WWDC)에서 ‘맥 스튜디오 ',
+      link: 'https://n.news.naver.com/mnews/article/018/0005502153?sid=105',
+      regDtm: '2023-06-07',
+      author: '이데일리',
+      site: '네이버',
+    },
+    {
+      contentId: 163,
+      siteCode: null,
+      title: '카카오 빼고는 적자…모빌리티 버티기 통할까',
+      description:
+        '이른바 ‘타다 금지법’ 등 모빌리티 업계 혁신을 저해하는 각종 규제 및 시장경쟁 과열로 국내 모빌리티 사업자 대부분이 영업손실을 …',
+      link: 'https://n.news.naver.com/mnews/article/011/0004198702?sid=105',
+      regDtm: '2023-06-07',
+      author: '서울경제',
+      site: '구글',
+    },
+    {
+      contentId: 164,
+      siteCode: null,
+      title: "'카카오톡 조용히 나가기', 3주간 200만명 사용",
+      description:
+        "카카오톡(카톡)이 선보인 '단체방 조용히 나가기' 기능을 이용한 사용자가 3주간 200만명이 넘은 것으로 나타났다. 6일 카카오 …",
+      link: 'https://n.news.naver.com/mnews/article/031/0000750405?sid=105',
+      regDtm: '2023-06-07',
+      author: '아이뉴스24',
+      site: '카카오',
+    },
+  ]
+
   Apis.getContents().then((data) => {
     console.log(data)
   })
@@ -44,60 +80,33 @@ export default function Main() {
         </section>
         <section className="mt-12">
           <ul>
-            <li className="border-t first:border-0">
-              <a href="#" className="block py-8 cursor-pointer">
-                <div className="flex items-center">
-                  <strong className="rounded-full bg-pink-50 px-3 py-1.5 font-medium text-pink-600">
-                    네이버
-                  </strong>
-                  <span className="text-sm text-gray-400 ml-4">이데일리</span>
-                  <span className="text-sm text-gray-400 before:content-['|'] before:mx-3 before:text-gray-200">
-                    2023-06-23
-                  </span>
-                </div>
-                <p className="mt-4 text-2xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600 break-keep">
-                  티빙, 국내 OTT 중 상반기 MAU 가장 많이 올랐다
-                </p>
-                <p className="mt-3 line-clamp-2 text-sm leading-6 text-gray-600 break-keep">
-                  테슬라, 트위터, 스페이스X의 최고경영자(CEO)인 일론 머스크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커
-                </p>
-              </a>
-            </li>
-            <li className="border-t py-8 first:border-0">
-              <a href="#" className="block cursor-pointer">
-                <div className="flex items-center">
-                  <strong className="rounded-full bg-pink-50 px-3 py-1.5 font-medium text-pink-600">
-                    네이버
-                  </strong>
-                  <span className="text-sm text-gray-400 ml-4">이데일리</span>
-                  <span className="text-sm text-gray-400 before:content-['|'] before:mx-3 before:text-gray-200">
-                    2023-06-23
-                  </span>
-                </div>
-                <p className="mt-4 text-2xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600 break-keep">
-                  티빙, 국내 OTT 중 상반기 MAU 가장 많이 올랐다
-                </p>
-                <p className="mt-3 line-clamp-2 text-sm leading-6 text-gray-600 break-keep">
-                  테슬라, 트위터, 스페이스X의 최고경영자(CEO)인 일론 머스크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커크와
-                  페이스북의 모회사 메타플랫폼(이하 메타)의 CEO 마크 저커
-                </p>
-              </a>
-            </li>
+            {data.map((item) => (
+              <li key={item.contentId} className="border-t py-8 first:border-0">
+                <a
+                  href={item.link}
+                  target="_blank"
+                  className="block cursor-pointer"
+                >
+                  <div className="flex items-center">
+                    <strong className="rounded-full bg-pink-50 px-3 py-1.5 font-medium text-pink-600">
+                      {item.site}
+                    </strong>
+                    <span className="text-sm text-gray-400 ml-4">
+                      {item.author}
+                    </span>
+                    <span className="text-sm text-gray-400 before:content-['|'] before:mx-3 before:text-gray-200">
+                      {item.regDtm}
+                    </span>
+                  </div>
+                  <p className="mt-4 text-2xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600 break-keep">
+                    {item.title}
+                  </p>
+                  <p className="mt-3 line-clamp-2 text-sm leading-6 text-gray-600 break-keep">
+                    {item.description}
+                  </p>
+                </a>
+              </li>
+            ))}
           </ul>
         </section>
         <div className="flex justify-center mt-14">
