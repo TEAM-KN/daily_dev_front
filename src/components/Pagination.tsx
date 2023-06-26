120fa565c5743bf27c57fa523a0608522dc27b3a
kkomyoung <kya754@gmail.com>
2023-06-27 00:19:54 +0900

    #13 feat: Pagination, PoistLIst css 스타일 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -26,7 +26,7 @@ function IndexButton({
         <button
           type="button"
           onClick={onClick}
-          className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 transition duration-200"
         >
           {indexNumber}
         </button>
@@ -44,7 +44,7 @@ function GoToPreviousButton({ goToPreviousIndex }: GoToPreviousButtonProps) {
     <button
       type="button"
       onClick={goToPreviousIndex}
-      className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 transition duration-200"
     >
       <span className="sr-only">이전</span>
       <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -60,7 +60,7 @@ function GoToNextButton({ goToNextIndex }: GoToNextButtonProps) {
   return (
     <button
       onClick={goToNextIndex}
-      className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 transition duration-200"
     >
       <span className="sr-only">다음</span>
       <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NewspaperIcon } from '@heroicons/react/24/solid'
 
 type PostListProps = {
   currentPosts: Content[]
@@ -7,26 +8,55 @@ type PostListProps = {
 type Content = {
   contentId: number
   link: string
-  site: string
+  siteName: string
+  siteCode: string
   author: string
   regDtm: string
   title: string
   description: string
 }
 
+function NoPostList() {
+  return (
+    <div className="text-center py-36">
+      <NewspaperIcon className="inline w-10 text-gray-300" />
+      <strong className="text-xl font-semibold block mt-3">
+        아직 준비 중이에요
+      </strong>
+      <p className="text-sm text-gray-400 mt-1">조금만 기다려주세요!</p>
+    </div>
+  )
+}
+
 export default function PostList({ currentPosts }: PostListProps) {
+  const color = (siteName: string) => {
+    const colorMap: { [key: string]: string } = {
+      NAVER: 'bg-green-50 text-green-700',
+      KAKAO: 'bg-yellow-50 text-yellow-800',
+      DAANGN: 'bg-orange-50 text-orange-600',
+      WOO: 'bg-blue-50 text-blue-700',
+    }
+
+    return colorMap[siteName] || ''
+  }
+
   return (
     <>
+      {currentPosts.length < 1 && <NoPostList />}
       {currentPosts.map((content: Content) => (
         <li key={content.contentId} className="border-t first:border-0">
           <a
             href={content.link}
             target="_blank"
-            className="block py-8 px-6 cursor-pointer hover:bg-gray-50 transition duration-300"
+            className="block py-8 px-6 cursor-pointer hover:bg-gray-50 transition duration-200"
           >
             <div className="flex items-center">
-              <strong className="rounded-full bg-pink-50 px-3 py-1.5 font-medium text-pink-600">
-                {content.site}
+              <strong
+                className={`rounded-full px-3 py-1.5 font-medium ${color(
+                  content.siteCode,
+                )}`}
+              >
+                {content.siteName}
               </strong>
               <span className="text-sm text-gray-400 ml-4">
                 {content.author}
