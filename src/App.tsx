ebb99a038fd350a5dee94908fdb8a2472356652e
kkomyoung <kya754@gmail.com>
2023-06-26 12:54:20 +0900

    #13 feat: react-query 설치

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import React from 'react'
 // routes
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './router'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename="/">
+        <Routes />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
