d704e9d5f8443bffa2bbc054118be162965aaa55
kkomyoung <kya754@gmail.com>
2023-07-05 16:38:05 +0900

    #3 feat: 이메일 중복확인 api 추가

--- src/service/apis.ts
+++ src/service/apis.ts
@@ -17,3 +17,11 @@ export const getContentsOfSite = async (site: string): Promise<any> => {
   const { data } = await customAxios.get(`/content/site?siteCode=${site}`)
   return data
 }
+
+// 회원가입
+
+// 이메일 중복확인
+export const getAuthIsCheck = async (email: string): Promise<any> => {
+  const { data } = await customAxios.get(`/auth/isCheck?email=${email}`)
+  return data
+}
