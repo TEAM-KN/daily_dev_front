161e49f67da4c484ba0245b073412d2449217fec
kkomyoung <kya754@gmail.com>
2023-07-10 23:34:17 +0900

    #27 feat: 회원가입 완료화면 텍스트 수정

--- src/view/RegisterComplete.tsx
+++ src/view/RegisterComplete.tsx
@@ -6,21 +6,24 @@ export default function RegisterComplete() {
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <Header />
       <div className="text-center py-32 sm:py-48 lg:py-50">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
+        <h1 className="text-5xl sm:text-6xl font-bold text-gray-900">
+          WELCOME!
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-6 text-md sm:text-lg text-gray-600 break-keep leading-7 sm:leading-8">
+          꼼영님, 데일리데브 회원가입을 축하합니다.
+          <br />
+          선택하신 구독 서비스를{' '}
+          <span className="text-indigo-600">kya754@daily.dev</span>로
+          보내드리겠습니다.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            href="/main"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            처음으로
+            시작하기
           </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
+          <a href="#" className="text-md font-semibold text-gray-900">
             구독 서비스 변경하기 <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
