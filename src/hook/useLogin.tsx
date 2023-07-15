af5555db7e5232bb2cdbc416a22331188817e722
kkomyoung <kya754@gmail.com>
2023-07-15 22:04:09 +0900

    feat: 회원정보 조회 추가

--- src/hook/useLogin.tsx
+++ src/hook/useLogin.tsx
@@ -1,27 +1,41 @@
-import React, { useState } from 'react'
-import { useMutation } from 'react-query'
+import React, { useState, useEffect } from 'react'
+import { useMutation, useQuery } from 'react-query'
 import { useNavigate } from 'react-router-dom'
-import { getAuthLogin } from '../service/apis'
+import { getAuthLogin, getUserInfo } from '../service/apis'
 
 export const useLogin = () => {
+  const [email, setEmail] = useState<string>('')
   const [errorMessage, setErrorMessage] = useState<string>('')
   const navigate = useNavigate()
 
-  const { mutate, isLoading } = useMutation(getAuthLogin, {
+  // 로그인 API
+  const { mutate, isLoading } = useMutation(getAuthLogin)
+  // 회원정보 조회 API
+  const { refetch } = useQuery(['userInfo', email], () => getUserInfo(email), {
+    enabled: false,
     onSuccess: (data) => {
-      console.log(data)
-      navigate('/')
       localStorage.setItem('email', data.email)
       localStorage.setItem('nickname', data.nickname)
-      localStorage.setItem('access-token', 'token')
-    },
-    onError: () => {
-      setErrorMessage('이메일이나 비밀번호를 다시 확인해주세요')
+      localStorage.setItem('access-token', 'test token')
+      navigate('/main')
     },
   })
 
+  useEffect(() => {
+    if (email) {
+      refetch()
+    }
+  }, [email])
+
   const login = (data: any) => {
-    mutate(data)
+    mutate(data, {
+      onSuccess: () => {
+        setEmail(data.email)
+      },
+      onError: () => {
+        setErrorMessage('이메일이나 비밀번호를 다시 확인해주세요')
+      },
+    })
   }
   return { login, isLoading, errorMessage, setErrorMessage }
 }
